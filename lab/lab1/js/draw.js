bd651a88b7e25115907033cd256a34e2c1ea56a8
Jalilg <35570605+Jalilg@users.noreply.github.com>
2021-04-02 21:47:59 -0400

    Update draw.js

--- lab/lab1/js/draw.js
+++ lab/lab1/js/draw.js
@@ -110,4 +110,15 @@ map.on('draw:created', function (e) {
     var type = e.layerType; // The type of shape
     var layer = e.layer; // The Leaflet layer for the shape
     var id = L.stamp(layer); // The unique Leaflet ID for the layer
+    var theHtml = `<div class="shape" data-leaflet-id=${id}><h1>Current ID: ${id}</h1></div>`
+    
+    if ( ! _.isUndefined (myRectangle) ) { // if myRectangle is defined
+      map.removeLayer(myRectangle)
+    }
+    myRectangle = layer
+    map.addLayer(myRectangle)
+
+    $('#shapes').html(`${theHtml}`)
+      
+    
 });
